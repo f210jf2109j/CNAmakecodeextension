e12a1ed467d4fd8b4cfe2646225386d627808c16
KarmaKoin <89550400+KarmaKoin@users.noreply.github.com>
2023-12-02 03:47:55 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -309,6 +309,29 @@ namespace CodeNinjasAbbotsford {
     }
 
     /**
+     * After a certain amount of time, the attached code will run.
+     * Blocks after this one will run without waiting.
+     */
+    //% block="after $time do"
+    //% time.defl=500
+    //% handlerStatement=1
+    //% %time=timePicker ms"
+    export function after(time: number, thenDo: () => void) {
+        setTimeout(thenDo, time)
+    }
+
+    /**
+     * Run the attached code seperately from other code.
+     * This creates a seperate context for "pause" so that pauses
+     * within or without this code are seperated.
+     */
+    //% block="separately do"
+    //% handlerStatement=1
+    export function background(then: () => void) {
+        control.runInBackground(then)
+    }
+
+    /**
      * Game Made By The CNA sensei's!
      * This Block Does Nothing...
      */
