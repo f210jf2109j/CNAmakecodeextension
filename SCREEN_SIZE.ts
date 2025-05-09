ca914229f48b4cd193e652ae490738aa18165bbe
f210jf2109j <richard.liang@millburn.org>
2025-05-09 14:29:34 -0400

    Update SCREEN_SIZE.ts

--- SCREEN_SIZE.ts
+++ SCREEN_SIZE.ts
@@ -4,6 +4,6 @@ namespace userconfig
     // Width & Height Of The Screen
     // Default Width: 160 - Default Height: 120
     
-/*Width*/  const ARCADE_SCREEN_WIDTH = 160
-/*Height*/ const ARCADE_SCREEN_HEIGHT = 120
+/*Width*/  const ARCADE_SCREEN_WIDTH = 320
+/*Height*/ const ARCADE_SCREEN_HEIGHT = 240
 }
